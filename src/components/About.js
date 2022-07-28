81cf355f04d0296148d241ee8be284c8ed6f260f
Michael <mickeymurage@gmail.com>
2022-07-28 14:06:07 +0300

    added a little info on About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { randomColor } from './randomColor'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+        
+        
+                <div id='title-form' className='card-form card' style={{width: '18rem', backgroundColor: randomColor(), marginTop: '2px'}}>
+                    <h2>Info about the app:</h2>
+                    <p> This is my version of a working notepad using our stubborn but beloved JavaScript (along with React.js).</p>
+                </div>
+                {/* <div id='textarea-form'>
+                    <textarea placeholder='Enter note here...' id='textarea-input' onChange={handleChange}>
+                        
+                    </textarea>
+                </div> */}
+    </div>
   )
 }
 
