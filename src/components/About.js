c12674ff292ec8b4cee85b83b619b53cbd86df98
Michael <mickeymurage@gmail.com>
2022-07-28 17:36:09 +0300

    Added content on About.js and Links.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,16 +3,23 @@ import { randomColor } from './utilities/randomColor'
 
 function About() {
   return (
-    <div style={{paddingTop: '4rem'}}>   
-        <div id='title-form' className='card-form card' style={{width: '18rem', backgroundColor: randomColor(), marginTop: '2px'}}>
-            <h2>Info about the app:</h2>
+    <div style={{paddingTop: '4rem'}} className='card-container'>   
+        <div className='card-form card about-form' style={{width: '18rem', backgroundColor: randomColor(), marginTop: '2px'}}>
+            <h2>General description:</h2>
                 <p> This is my version of a working notepad using our stubborn but beloved JavaScript (along with React.js).</p>
-            </div>
-            {/* <div id='textarea-form'>
-                <textarea placeholder='Enter note here...' id='textarea-input' onChange={handleChange}>
-                    
-                </textarea>
-            </div> */}
+        </div>
+        <div className='card-form card about-form' style={{width: '18rem', backgroundColor: randomColor(), marginTop: '2px'}}>
+            <h2>React version:</h2>
+                <p>Version 18.</p>
+        </div>
+        <div className='card-form card about-form' style={{width: '18rem', backgroundColor: randomColor(), marginTop: '2px'}}>
+            <h2>React router dom</h2>
+                <p> Version 6.</p>
+        </div>
+        <div className='card-form card about-form' style={{width: '18rem', backgroundColor: randomColor(), marginTop: '2px'}}>
+            <h2>Time of creation:</h2>
+                <p> July 2022.</p>
+        </div>
     </div>
   )
 }
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { randomColor } from './utilities/randomColor'
 
 function Links() {
   return (
-    <div>Links</div>
+    <div style={{paddingTop: '4rem'}} className='card-container'>
+        <div className='about-form card-form card' style={{width: '18rem', backgroundColor: randomColor(), marginTop: '2px'}}>
+            <h2>Link:</h2>
+                <Link style={{textDecoration: 'none'}} to='https://michael-murage.github.io/Portfolio/'>Portfolio</Link>
+        </div>
+        <div className='about-form card-form card' style={{width: '18rem', backgroundColor: randomColor(), marginTop: '2px'}}>
+            <h2>Other link:</h2>
+                <Link style={{textDecoration: 'none'}} to='https://michael-murage.github.io/phase-1-project-game-library/'>Game library</Link>
+        </div>
+        <p>Above are links to my portfolio and a simple game library project</p>
+    </div>
   )
 }
 
