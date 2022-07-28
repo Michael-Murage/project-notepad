59a9e688e9038f227daaffca89993927ff8d01ea
Michael <mickeymurage@gmail.com>
2022-07-28 10:50:04 +0300

    added useRef to enable edit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,41 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 function Card({ note, randomColor, deleteNote }) {
     const [isNone, setIsNone] = useState(false)
+    const [edit, setEdit] = useState(note.title)
     let conditionIsNone = isNone ? 'none' : 'block'
     let conditionNotNone = !isNone ? 'none' : 'block'
+    const focusRef = useRef()
+    // focusRef.focus();
+
+    useEffect(()=>{
+        focusRef.current.focus()
+    }, [isNone])
     
     function offFocus(){
-        // setIsNone(false)
+        setIsNone(false)
     }
 
     function changeTitle(e){
         setIsNone(true)
+        
     }   
+
+    function submitEdit(e){
+        
+    }
+
+    function controlEdit(e){
+        setEdit(e.target.value)
+    }
 
   return (
     <div className='card'  style={{width: '18rem', backgroundColor: randomColor()}}>
                 <div style={{display: 'flex', justifyContent: 'space-between'}}>
                     <h2 onClick={changeTitle} className={conditionIsNone + ' ' + note?.id}>{!note.title ? 'Loading...' : note.title}</h2>
-                    <input type='text' id='title-input' onBlur={offFocus} placeholder='Enter text' className={conditionNotNone + ' ' + note?.id}/>
+                    <form onSubmit={submitEdit} >
+                        <input onChange={controlEdit} ref={focusRef} type='text' id='title-input' onBlur={offFocus} value={edit} className={conditionNotNone + ' ' + note?.id} autoFocus />
+                    </form>
                     <button className='delete-note' onClick={() => deleteNote(note?.id)}>X</button>
                 </div>
                 <p>{!note.content ? 'Loading...' : note.content}</p>
