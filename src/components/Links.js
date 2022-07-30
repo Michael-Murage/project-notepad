8344abb09d71d03a4ecc928d9e5c90738b39c683
Michael <mickeymurage@gmail.com>
2022-07-30 14:14:36 +0300

    fixed links in links page

--- src/components/Links.js
+++ src/components/Links.js
@@ -7,11 +7,11 @@ function Links() {
     <div style={{paddingTop: '4rem'}} className='card-container'>
         <div className='about-form card-form card' style={{width: '18rem', backgroundColor: randomColor(), marginTop: '2px'}}>
             <h2>Link:</h2>
-                <Link style={{textDecoration: 'none'}} to='https://michael-murage.github.io/Portfolio/'>Portfolio</Link>
+                <a style={{textDecoration: 'none'}} href='https://michael-murage.github.io/Portfolio/'>Portfolio</a>
         </div>
         <div className='about-form card-form card' style={{width: '18rem', backgroundColor: randomColor(), marginTop: '2px'}}>
             <h2>Other link:</h2>
-                <Link style={{textDecoration: 'none'}} to='https://michael-murage.github.io/phase-1-project-game-library/'>Game library</Link>
+                <a style={{textDecoration: 'none'}} to='https://michael-murage.github.io/phase-1-project-game-library/'>Game library</a>
         </div>
         <p>Above are links to my portfolio and a simple game library project</p>
     </div>
